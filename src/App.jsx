8621baa17ff5fdc60271995d5c05c0845a552618
yenvy0709 <151055632+yenvy0709@users.noreply.github.com>
2023-11-23 08:54:18 +0000

    Edit Text, Update package.json to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Yến Vy",
+  title: "Web Developer",
+  email: "vy@gmail.com",
+  gitHub: "vy",
+  instagram: "vy",
+  linkedIn: "vy",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "vy",
+  youTube: "vy",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -23,8 +23,7 @@ const imageAltText = "purple and blue abstract background";
 /**
  * Sort description that expands on your title on the Home component.
  */
-const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+const description = "I'm a web developer and I'm learning to become a senior developer";
 
 /**
  * List of some of skills or technologies you work on, are learning,
